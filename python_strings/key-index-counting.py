74f3cc3d606ffc026ddf44f3380101f37126343e
Andrés Laurito Giber <andy.laurito@gmail.com>
2025-02-23 12:30:46 +0100

    Cormen index counting

--- python_strings/key-index-counting.py
+++ python_strings/key-index-counting.py
@@ -66,4 +66,25 @@ from the first element to set up to the last one, you go backwards in Cormen
 
 IMPORTANT FACT of the above: You need to implement the last loop backwards
 to keep stability!
+
+Why the above? Think of it by taking a as example.
+If u start forward, u pick the first a u have and u put it in the LAST position of the a's. Then u grab your second a and u put it in the before last position and so on. By doing this, you are inverting the order of the elements
 """
+
+a = ['d', 'a', 'c', 'f', 'f', 'b', 'd', 'b', 'f', 'b', 'e', 'a']
+n = len(a)
+count_cormen = [0] * r
+aux = [0] * n
+
+for i in range(n):
+    count_cormen[mapping.get(a[i])] += 1
+
+for i in range(1, r):
+    count_cormen[i] += count_cormen[i - 1]
+
+for i in reversed(range(n)):
+    aux[count_cormen[mapping.get(a[i])] - 1] = a[i]
+    count_cormen[mapping.get(a[i])] -= 1
+
+for i in range(n):
+    a[i] = aux[i]
