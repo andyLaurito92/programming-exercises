33c679156528742fc64f0e08f0d0cb7d8de2d768
Andrés Laurito Giber <andy.laurito@gmail.com>
2025-03-19 10:39:35 +0100

    Removing first solution as it's not correct

--- meta/rotary_lock.py
+++ meta/rotary_lock.py
@@ -19,58 +19,6 @@ Constraints
 
 """
 
-# Write any import statements here
-def leftDistance(curr:int, to:int, N: int) -> int:
-  #1. Distance from curr to N. We know that always there's 1
-  # meaning distance from 1 to N is 1
-  res = curr - 1 # distance to the beginning
-  if to == 1:
-    return res
-  elif to == N:
-    return res + 1
-  else:
-    res += 1 # Distance from 1 to N
-    res += N - to
-  return res
-
-def rightDistance(curr:int, to:int, N:int) -> int:
-  return abs(curr - to)
-
-def getMinCodeEntryTime(N: int, M: int, C: list[int]) -> int:
-  curr = 1  
-  res = 0
-  memory = {}
-  for c in C:
-    if memory.get((curr,c)) is not None:
-        res += memory.get((curr, c))
-    else:
-      left = leftDistance(curr, c, N)
-      right = rightDistance(curr, c, N)
-      if  left < right:
-        res += left
-        memory[(curr,c)] = left
-      else:
-        res += right
-        memory[(curr,c)] = right
-    curr = c
-  return res
-
-
-assert 1 == leftDistance(1, 10, 10)
-assert 4 == leftDistance(1, 7, 10)
-assert 6 == leftDistance(4, 8, 10)
-assert 2 == leftDistance(1, 9, 10)
-
-assert 2 == getMinCodeEntryTime(3, 3, [1, 2, 3])
-assert 11 == getMinCodeEntryTime(10, 4, [9, 4, 4, 8])
-
-assert 0 == getMinCodeEntryTime(100, 1, [1])
-assert 0 == getMinCodeEntryTime(100, 10, [1] * 10)
-
-
-"""
-Second solution, way simpler using arithmetic module
-"""
 def getMinCodeEntryTime2(N: int, M: int, C: list[int]) -> int:
   curr = 1
   res = 0
