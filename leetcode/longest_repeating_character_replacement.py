97f8c43963295fcb6e3d41c8e2fe9070814d3100
Andrés Laurito Giber <andy.laurito@gmail.com>
2025-05-08 17:14:08 +0200

    sliding window technique

--- leetcode/longest_repeating_character_replacement.py
+++ leetcode/longest_repeating_character_replacement.py
@@ -52,6 +52,16 @@ def characterReplacementNaive(s: str, k: int) -> int:
 
 from string import ascii_uppercase
 
+"""
+This solution uses a prefix array to calculate the char count
+ending at string i. We then just substract the count for substr[i:j]
+, calculate the most frequent character there and finally do the same
+as above
+
+Runtime: O(N^2 * r)
+Space: O(N*r)
+n = len(s), r = 26 = len(ascii_uppercase)
+"""
 def characterReplacement2(s: str, k: int) -> int:
     n = len(s)
     r = len(ascii_uppercase)
@@ -74,7 +84,7 @@ def characterReplacement2(s: str, k: int) -> int:
         if i == 0:
             return maxcharcount[j]
 
-        start = maxcharcount[i]
+        start = maxcharcount[i-1]
         end = maxcharcount[j]
         res = [0] * r
 
@@ -82,7 +92,6 @@ def characterReplacement2(s: str, k: int) -> int:
             res[m] = end[m] - start[m]
 
         return res
-
 
     res = 0
     for i in range(n):
@@ -94,14 +103,55 @@ def characterReplacement2(s: str, k: int) -> int:
                     max_freq = charcount[m]
 
             len_substr = (j - i + 1)
-            to_replace = len_substr - max_freq
-            if to_replace <= k:
+            if max_freq == len_substr:
+                # Substring w/all same characters
                 res = max(len_substr, res)
+            else:
+                to_replace = len_substr - max_freq
+                if to_replace <= k:
+                    res = max(len_substr, res)
+    return res
+
+"""
+In this approach, we implemenet a sliding window technique
+"""
+from operator import itemgetter
+
+
+def characterReplacementSlidingWindow(s: str, k: int) -> int:
+    n = len(s)
+    if n < 2:
+        return n
+
+    res = 0
+    start = 0
+    state = {}
+    for end in range(n):
+        state[s[end]] = state.get(s[end], 0) + 1
+
+        while len(state.keys()) > 1:
+            letter, max_freq = max(state.items(), key=itemgetter(1))
+            diff_chars = sum(v for c, v in state.items() if c != letter)
+            if diff_chars <= k:
+                # the amount of different characters is still less or equal
+                # than k, meaning I can change them to still fulfill the condition
+                break
+
+            # reduce the window until condition is matched again
+            count = state.get(s[start])
+            if count - 1 == 0:
+                del state[s[start]]
+            else:
+                state[s[start]] = count - 1
+            start += 1
+
+        res = max(res, sum(state.values()))
     return res
 
 
-for fun in [characterReplacementNaive, characterReplacement2]:
+for fun in [characterReplacementNaive, characterReplacement2, characterReplacementSlidingWindow]:
     assert 4 == fun("ABAB", 2), f"Failed w/fn {fun}, input: ABAB k = 2"
     assert 3 == fun("AAAB", 0)
     assert 4 == fun("AABABBA", 1)
     assert 4 == fun("ABBB", 2)
+    assert 2 == fun("ABAA", 0), f"Failed w/fn {fun}, input: ABAA k = 2"
