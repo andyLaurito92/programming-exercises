02466d019f08308ca2ed89edd770a538f3affdc7
Andrés Laurito Giber <andy.laurito@gmail.com>
2025-06-04 12:58:35 +0200

    Improving ispalindrome so we don't create a new substr per each search

--- leetcode/longest_palindromic_substring.py
+++ leetcode/longest_palindromic_substring.py
@@ -8,9 +8,9 @@ Constraints:
 
 """
 
-def ispalindrome(mystring: str) -> bool:
-    i = 0
-    j = len(mystring) - 1
+def ispalindrome(mystring: str, start:int, end:int) -> bool:
+    i = start
+    j = end - 1
     while i <= j:
         if mystring[i] != mystring[j]:
             return False
@@ -35,7 +35,7 @@ def longestPalindrome1(s: str) -> str:
     res = None
     for i in range(n):
         for j in range(i, n+1):
-            if ispalindrome(s[i:j]):
+            if ispalindrome(s, i, j):
                 if res is None or j - i + 1 > res[1] - res[0] + 1:
                     res = (i, j)
 
@@ -61,7 +61,11 @@ Why? Because we are iterating from the longest length to the shortest one, there
 the first palindrome that we find is gonna be the longest (there might be others of
 the same length, but not of more length, otherwise, we should have found it in a
 previous iteration)
+
+Runtime: O(N^3)
+Space: O(1)
 """
+
 def longestPalindrome2(s: str) -> str:
     n = len(s)
     if n <= 1:
@@ -70,12 +74,97 @@ def longestPalindrome2(s: str) -> str:
     for length in reversed(range(1, n+1)):
         for i in range(0, n - length + 1):
             j = i + length
-            if ispalindrome(s[i:j]):
+            if ispalindrome(s, i, j):
                 return s[i:j]
     return ''
 
+"""
+In order to improve our above algorithm, we need to be smarter on the way we
+calculate isPalindrome. Let's first start w/dyanmic programming and memoising
+s[i:j]
+"""
+from collections import defaultdict
 
-functions = [longestPalindrome1, longestPalindrome2]
+def longestPalindrome3(s: str) -> str:
+    n = len(s)
+    if n <= 1:
+        return s
+
+    # we represent [start][length]. Because length = 0 doens't make sense
+    # we need to create a n*(n+1) matrix
+    memory = [[False] for _ in range(n+1)] * n
+
+    for i in range(n):
+        memory[i][1] = True # strings of length 1 are palindromes
+
+    res = None
+    # babad
+    # xc0c1.cn/2-1cn/2cn/2-1...c1c0y
+    for length in range(2, n):
+        for i in range(0, n - length + 1):
+            center = i + (i + length) // 2
+            if memory[i][length - 1] and s[i-1] == s[length+1]:
+                memory[i-1][length+1] = True
+
+
+from typing import Optional
+
+
+"""
+Expanding around centers
+
+Runtime: O(N^2)
+Space: O(1)
+"""
+def longestPalindrome4(s: str) -> str:
+    n = len(s)
+    if n <= 1:
+        return s
+
+    res = None
+    def checkpalindrome(left: int, right: int):
+        res = None
+        while left >= 0 and right <= n - 1 and s[left] == s[right]:
+            left -= 1
+            right += 1
+        # left + 1 and right -1 was the last valid palindrome
+        left += 1
+        right -= 1
+        if s[left] != s[right]:
+            # no valid palindrome found
+            return None
+
+        if res is None or res[1] - res[0] + 1 < right - left + 1:
+            res = (left, right)
+        return res
+
+    def update(new_pal: tuple[int, int], res: Optional[tuple[int, int]]):
+        if new_pal is None:
+            return res
+        elif res is None:
+            return new_pal
+        elif new_pal[1] - new_pal[0] + 1 > res[1] - res[0] + 1:
+            return new_pal
+        else:
+            return res
+
+    for i in range(n - 1):
+        new_pal = checkpalindrome(i, i)
+        res = update(new_pal, res)
+
+        new_pal = checkpalindrome(i, i+1)
+        res = update(new_pal, res)
+
+    return s[res[0]:res[1]+1] if res else s[0:1]
+
+
+"""
+TODO: There is a trick for solving this in O(N) but it's not trivial at all. Is more
+for competitive programming. The algorithm is called Manacher's algorithm
+"""
+
+
+functions = [longestPalindrome1, longestPalindrome2, longestPalindrome4]
 for fun in functions:
     assert "bab" == fun("babad")
     assert "bb" == fun("cbbd")
