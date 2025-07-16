1988c80a3e10c036c1c975991800a1775fbdd412
andylaurito92 <andres.lauritogiber@bayer.com>
2025-07-16 17:08:22 +0200

    Adding exercise to solve

---
+++ interviews/lru.py
@@ -0,0 +1,11 @@
+"""
+Design and implement a data structure for Least Recently Used (LRU) cache. 
+It should support the following operations: get(key) and put(key,value) where key is an integer and value is a positive integer.
+ 
+ 
+get(key) Get the value of the key if the key exists in the cache, otherwise return something indicating that it’s missing.
+put(key, value) Set the value if the key is not already present or update it if it is. When the cache is at capacity, (i.e., it is full), it should evict (i.e., remove) the Least Recently Used item before inserting a new item.
+ 
+ 
+The cache is initialized with a positive capacity
+"""
